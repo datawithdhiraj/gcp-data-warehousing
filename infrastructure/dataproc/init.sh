44fece87ec75608e0136baf8eda6673e54e3d5f9
datawithdhiraj <dhiraj.dhande.de@gmail.com>
2026-04-27 16:25:13 +0530

    just added required dep

--- infrastructure/dataproc/init.sh
+++ infrastructure/dataproc/init.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-set -e
-set -x
+
+# set -e
+# set -x
 
 echo "Starting initialization..."
 
-# Avoid long apt update (optional but safer)
-apt-get update -y -qq
+# # Avoid long apt update (optional but safer)
+# apt-get update -y -qq
 
-# Ensure pip3 exists
-apt-get install -y python3-pip -qq
+# # Ensure pip3 exists
+# apt-get install -y python3-pip -qq
 
-# Upgrade pip quickly
-pip3 install --upgrade pip --no-cache-dir
+# # Upgrade pip quickly
+# pip3 install --upgrade pip --no-cache-dir
 
-# Install only required libs (fast)
+# # Install only required libs (fast)
 pip3 install --no-cache-dir google-cloud-secret-manager
 pip3 install --no-cache-dir requests
 
